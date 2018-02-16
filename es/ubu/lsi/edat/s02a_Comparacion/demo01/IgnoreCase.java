71dd3a209c070936b4af4980753473cb62015c20
Bruno Baruque <bbaruque@ubu.es>
2018-02-16 20:10:56 +0100

    Revisada indentación

--- es/ubu/lsi/edat/s02a_Comparacion/demo01/IgnoreCase.java
+++ es/ubu/lsi/edat/s02a_Comparacion/demo01/IgnoreCase.java
@@ -6,15 +6,15 @@ import java.util.Comparator;
  * Illustrates use of Comparator for ignoring case distinctions.
  */
 public class IgnoreCase implements Comparator<String> {
-	
-    /**
+
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	public int compare( String s1, String s2 ) {
-        return s1.compareToIgnoreCase( s2 );
-    }
-	
+		return s1.compareToIgnoreCase( s2 );
+	}
+
 }
